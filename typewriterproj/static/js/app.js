1193d2879dfbc270c990fcbc5be0836b507b9745
lizzie <elizabeth.j.quinlan@gmail.com>
2023-01-24 16:31:23 -0800

    add progress bar

--- typewriterproj/static/js/app.js
+++ typewriterproj/static/js/app.js
@@ -3,18 +3,28 @@ new Vue({
     el: '#app',
     delimiters: ['[[', ']]'],
     data: {
-        dailyWC: 1,
+        dailyWC: 0,
         // v-model to the textarea in html
         textArea: "",
-        totalcharacter: 0,
+        totalWord: 0,
+        // TODO: Later become a user-submitted value
+        dailyGoal: 130,
+        progress: null,
     },
     methods: {
         countText() {
-            console.log("here we are!")
-            this.totalcharacter = this.textArea.match(/[\w\d\’\'-]+/gi);//
-            this.dailyWC = this.totalcharacter.length
+            // Determining what counts as a word based on regex
+            this.totalWord = this.textArea.match(/[\w\d\’\'-]+/gi);
+            // need to add in error handling for value that is less than 1
+            // length of totalCharacter becomes null and this throws type error
+            this.dailyWC = this.totalWord.length
         },
     },
     computed: {
+        calcProgress() {
+            // Calculates percentage of your progress toward goal
+            this.progress = Math.round(this.dailyWC/this.dailyGoal*100)
+            return this.progress
+        }
     }
 })
