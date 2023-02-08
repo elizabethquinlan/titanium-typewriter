b733a422e1fdb7db7912efd8b5b1c180dfa970cf
lizzie <elizabeth.j.quinlan@gmail.com>
2023-02-08 10:11:34 -0800

    update serializer for handling create or update of project upon posting day's wc

--- typewriterproj/apis/serializers.py
+++ typewriterproj/apis/serializers.py
@@ -21,22 +21,15 @@ class DailyWcSerializer(serializers.ModelSerializer):
     class Meta:
         model = DailyWc
         fields = '__all__'
-        # (
-            # 'project',
-            # 'user',
-            # 'daily_goal',
-            # 'daily_goal_bool',
-            # 'todays_wc',
-            # 'text_area',
-            # 'date',
-            # 'accessed_today',
-            # 'id',
-        # )
 
     # For the nested serializer to be writable, you'll need to create create() and/or update() methods
     # to explicitly specify how the child relationships should be saved.
     def create(self, validated_data):
         project_data = validated_data.pop('project')
-        project = Project.objects.create(**project_data)
+        try:
+            project = Project.objects.get(name='Unassigned')
+        except Project.DoesNotExist:
+            project = Project.objects.create(**project_data)
         dailywc = DailyWc.objects.create(project=project, **validated_data)
+        print(f'{dailywc} is the daily wordcount. Project data: {project_data}. Project: {project}')
         return dailywc
